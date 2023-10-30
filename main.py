6b2de5512a926b5e2d9b07ddbb34df92b3e95677
AdryanMaikel <adryanmaikel@gmail.com>
2023-10-30 16:32:06 -0300

    melhorando codigo

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 from geopy.geocoders import Nominatim
 import pandas as pd
 
-geolocator = Nominatim(user_agent="geoapiExercises")
+geolocator = Nominatim(user_agent="Adryan")
 data = pd.read_excel('coord.xlsx')
 # print(data.head(0))
-latitude = data['Lat']
-longitude = data['Lon']
+latitude = data['LAT']
+longitude = data['LONG']
 count = 0
 addresss = []
 for la, lo in zip(latitude, longitude):
-  count = count + 1
+  count += 1
   location = geolocator.reverse(f"{la}, {lo}")
-  if location:
-    address = location.address
-  else:
-    print("Não foi possível obter o endereço. Verifique as coordenadas.")
-  print(f"{count} = {address}")
-  addresss.append(f"{address}")
-  
+  address = location.address if location else print("Verifique as coordenadas.")
+  print(f"{count} = {address.split(', Porto Alegre,')[0]}")
+  addresss.append(f"{address.split(', Porto Alegre,')[0]}")
+
 data['Local'] = addresss
 print(data.head())
 name = 'dados.xlsx'
